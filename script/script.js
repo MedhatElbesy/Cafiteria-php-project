3e2a1d3dc002916725f0ecbbba4ffc209f5b3608
Khaled Abdelbaset <Khaledabdelbaset98@gmail.com>
2024-03-08 23:21:59 +0200

    updating admin page

--- script/script.js
+++ script/script.js
@@ -5,6 +5,9 @@ let allProducts = [
   { "id": 3, "img": "../images/landing-img.jpg", "name": "Cola", "price": 15, "category_id": 3},
   { "id": 4, "img": "../images/landing-img.jpg", "name": "Orange", "price": 20, "category_id": 4},
   { "id": 5, "img": "../images/landing-img.jpg", "name": "Mango", "price": 20, "category_id": 4},
+  { "id": 3, "img": "../images/landing-img.jpg", "name": "Cola", "price": 15, "category_id": 3},
+  { "id": 4, "img": "../images/landing-img.jpg", "name": "Orange", "price": 20, "category_id": 4},
+  { "id": 5, "img": "../images/landing-img.jpg", "name": "Mango", "price": 20, "category_id": 4},
 ];
 
 let allCategories = [
@@ -12,58 +15,64 @@ let allCategories = [
   { "id": 2, "img": "../images/landing-img.jpg", "name": "Cold Drinks"},
   { "id": 3, "img": "../images/landing-img.jpg", "name": "Soda"},
   { "id": 4, "img": "../images/landing-img.jpg", "name": "Juice"},
+  { "id": 4, "img": "../images/landing-img.jpg", "name": "Juice"},
+  { "id": 4, "img": "../images/landing-img.jpg", "name": "Juice"},
 ];
 
 let categoryContainer = document.getElementById("categories");
 let productsContainer = document.getElementById("products");
-let orderData = document.querySelector(".order-data");
-let bill = document.getElementById("bill");
-let lastOrder = document.getElementById("lastOrder");
-let showLastOrder = document.querySelector(".show-last-order");
-let showBill = document.querySelector(".show-bill");
-let allOrders = bill.getElementsByTagName("tbody")[0].children;
+let cart = document.getElementById("cart");
+let showcart = document.querySelector(".show-cart");
+let allOrders = cart.querySelector(".orders").children;
 
+let setCategory = function(i) {
+  let category = document.createElement("div");
+  let icon = document.createElement("i");
+  let p1 = document.createElement("p");
+  let p2 = document.createElement("p");
 
-let setCategory = function() {
-  for (let i =0; i < allCategories.length ; i++) {
-    let category = document.createElement("div");
-    let icon = document.createElement("i");
-    let p1 = document.createElement("p");
-    let p2 = document.createElement("p");
-  
-    category.classList.add("category", "py-3", "mx-1", "mb-2", "px-4", "text-center");
-    icon.classList.add("mb-2", "fa-solid", "fa-mug-hot");
-    p1.classList.add("m-0", "fw-bold");
-    p2.classList.add("m-0");
-    p1.innerText= allCategories[i].name;
-    p2.innerHTML="<span>200</span> Items";
-  
-    category.appendChild(icon);
-    category.appendChild(p1);
-    category.appendChild(p2);
-    categoryContainer.appendChild(category);
+  category.classList.add("category", "py-3", "mx-1", "mb-4", "px-4", "text-center");
+  icon.classList.add("mb-2", "fa-solid", "fa-mug-hot");
+  p1.classList.add("m-0", "fw-bold");
+  p2.classList.add("m-0");
+  p1.innerText= allCategories[i].name;
 
-    category.addEventListener("click", ()=>showCategoryProducts(allCategories[i]));
+  let items = 0;
+  for (let j = 0; j < allProducts.length; j++) {
+    if(allCategories[i].id == 0) {
+      items++;
+    }
+    else if (allProducts[j].category_id == allCategories[i].id) {
+      items++;
+    }
   }
-} ();
+  p2.innerHTML=`<span>${items}</span> Items`;
+
+  category.appendChild(icon);
+  category.appendChild(p1);
+  category.appendChild(p2);
+  categoryContainer.appendChild(category);
+  
+  category.addEventListener("click", ()=>showCategoryProducts(allCategories[i]));
+};
 
 let setOrder = function (productId) {
   for(let i = 0; i < allOrders.length; i++) {
-    let order = bill.getElementsByTagName("tbody")[0].children[i];
+    let order = cart.querySelector(".orders").children[i];
     if(order.dataset.orderId == productId) {
       addOrder(order, allProducts[productId]);
       return;
     }
   }
   
-  let newOrder = bill.getElementsByTagName("tbody")[0].insertRow();
-  newOrder.classList.add("order", "center", "justify-content-between", "p-2", "mb-2");
-  newOrder.dataset.quantity = 1;
+  let newOrder = document.createElement("div");
+  newOrder.classList.add("order", "center", "justify-content-between", "p-2", "m-1");
+  newOrder.dataset.quantity = 0;
   newOrder.dataset.orderId = productId;
   let quantity = Number(newOrder.dataset.quantity);
   
   for(let i = 0; i < 4; i++) {
-    newOrder.insertCell(0);
+    newOrder.appendChild(document.createElement("div"));
   }
 
   let image = document.createElement("img");
@@ -80,6 +89,8 @@ let setOrder = function (productId) {
     </div>`;
   orderPrice(newOrder, allProducts[productId]);
   newOrder.children[3].innerHTML = `<button id="cancelOrder" class="cancel reset center text-danger"><i class="fa-regular fa-circle-xmark"></i></button>`;
+  cart.querySelector(".orders").appendChild(newOrder);
+  addOrder(newOrder, allProducts[productId]);
 };
 
 let setProduct = function(i) {
@@ -89,7 +100,7 @@ let setProduct = function(i) {
   let p1 = document.createElement("p");
   let p2 = document.createElement("p");
   
-  product.classList.add("product", "m-0", "mb-2", "text-center");
+  product.classList.add("product", "mb-3", "text-center");
   product.setAttribute("id", "product");
   data.classList.add("data");
   p1.classList.add("product-name", "m-0", "p-2");
@@ -106,22 +117,17 @@ let setProduct = function(i) {
   data.appendChild(p2);
   product.appendChild(data);
   productsContainer.appendChild(product);
-
   product.addEventListener("click", function(){
-    showBill.classList.add("active");
-    showLastOrder.classList.remove("active");
-    bill.classList.remove("d-none");
-    lastOrder.classList.add("d-none");
     setOrder(i);
   });
 } ;
 
 // Order
-bill.addEventListener("click", function(e) {
-  let order = e.target.closest("tr");
+cart.addEventListener("click", function(e) {
+  let order = e.target.closest(".order");
   if(e.target.parentNode.classList.contains("cancel")) {
     order.remove();
-    // checkNoOrders();
+    checkOrderExistance();
   }
   if(e.target.classList.contains("add")) {
     addOrder(order, allProducts[order.dataset.orderId]);
@@ -136,6 +142,7 @@ let addOrder = function(order, product) {
   order.dataset.quantity = ++quantity;
   order.querySelector(".quantity").innerText = quantity;
   orderPrice(order, product);
+  checkOrderExistance();
 }
 
 let removeOrder = function(order, product) {
@@ -143,7 +150,7 @@ let removeOrder = function(order, product) {
   order.dataset.quantity = --quantity;
   if(quantity == 0) {
     order.remove();
-    // checkNoOrders();
+    checkOrderExistance();
     return;
   }
   order.querySelector(".quantity").innerText = quantity;
@@ -152,45 +159,37 @@ let removeOrder = function(order, product) {
 
 let orderPrice = function (order, product) {
   order.children[2].innerText = Number(order.dataset.quantity) * Number(product.price);
-  totalBill();
+  totalcart();
 }
 
-let totalBill = function() {
-  let billPrice = 0;
+let totalcart = function() {
+  let cartPrice = 0;
   for (let i = 0; i < allOrders.length; i++) {
-    billPrice += Number(allOrders[i].children[2].innerText);
+    cartPrice += Number(allOrders[i].children[2].innerText);
   }
-  bill.querySelector(".bill-price").getElementsByTagName("span")[1].innerHTML = billPrice;
+  cart.querySelector(".cart-price").getElementsByTagName("span")[1].innerHTML = cartPrice;
 };
 
-let showOrder = function(e) {
-  if (e.target.classList.contains("show-last-order")) {
-    e.target.classList.add("active");
-    showBill.classList.remove("active");
-    lastOrder.classList.remove("d-none");
-    bill.classList.add("d-none");
-  } else if (e.target.classList.contains("show-bill")) {
-    if(allOrders.length == 0) {
-      return;
-    }
-    e.target.classList.add("active");
-    showLastOrder.classList.remove("active");
-    bill.classList.remove("d-none");
-    lastOrder.classList.add("d-none");
+showcart.addEventListener("click", function(e) {
+  if(allOrders.length > 0) {
+    cart.classList.toggle("d-none");  
+  }
+});
+
+let checkOrderExistance = function () {
+  let container = document.getElementsByTagName("main")[0]
+  if(allOrders.length == 0) {
+    cart.classList.add("d-lg-none");
+    cart.classList.remove("d-lg-block");
+    container.classList.add("col-lg-12");
+    container.classList.remove("col-lg-9");
+  } else {
+    cart.classList.add("d-lg-block");
+    cart.classList.remove("d-lg-none");
+    container.classList.add("col-lg-9");
+    container.classList.remove("col-lg-12");
   }
 }
-orderData.addEventListener("click", e => showOrder(e));
-
-
-// let checkNoOrders = function() {
-//   if(allOrders.length == 0) {
-//     showLastOrder.classList.add("active");
-//     showBill.classList.remove("active");
-//     lastOrder.classList.remove("d-none");
-//     bill.classList.add("d-none");
-//   }
-// }
-
 // Category
 let showCategoryProducts = function(category) {
   let productsElement = document.getElementById("products");
@@ -200,7 +199,10 @@ let showCategoryProducts = function(category) {
 
   productsElement.previousElementSibling.innerText = category.name;
   for (let i = 0; i < allProducts.length; i++) {
-    if(allProducts[i].category_id == category.id) {
+    if(category.id == 0) {
+      setProduct(i);
+    }
+    else if(allProducts[i].category_id == category.id) {
       setProduct(i);
     }
   }
@@ -210,4 +212,12 @@ let ShowAllProducts = function() {
   for (let i = 0; i < allProducts.length; i++) {
     setProduct(i);
   }
+} ();
+
+let ShowCategories = function() {
+  allCategories.push({ "id": 0, "img": "../images/landing-img.jpg", "name": "All Products"})
+  for (let i = 0; i < allCategories.length ; i++) {
+    setCategory(i);
+  }
+
 } ();
