c188b8d233b0adbea9b41ea59ec9204e787c821c
MedhatElbesy <medhatelbesy@gmail.com>
2024-03-09 11:47:33 +0200

    edit user order

--- api/myorders.php
+++ api/myorders.php
@@ -7,9 +7,11 @@
     // for test enter this data
     $date_from = '2024-03-04';
     $date_to ='2024-03-07';
+    $customer_id='1';
     $data=$mydb->query(" SELECT *
         FROM orders 
-        WHERE order_date 
+        WHERE customers_id = '$customer_id'
+        AND order_date 
         BETWEEN '$date_from' AND '$date_to'
     ");
     $allOrdersInSpecificDate = $data->fetchAll(PDO::FETCH_ASSOC);
