ddb623a9b8ddf8b58e11f8194916cfbf7d3b7124
Your Name <m.abdelazem0202@gmail.com>
2024-03-16 00:34:28 +0200

    update style

--- script/showUsers.js
+++ script/showUsers.js
@@ -1,25 +1,25 @@
-const getUsers = async function(){
-    try {
-        const response = await fetch('../api/userAPI.php');
-        const json = await response.json();
-        usercard(json);
-        updateClickBtn();
-        deleteClickBtn();
-    } catch(error) {
-        console.log(error);
-    }
-}();
+const getUsers = (async function () {
+  try {
+    const response = await fetch("../api/userAPI.php");
+    const json = await response.json();
+    usercard(json);
+    updateClickBtn();
+    deleteClickBtn();
+  } catch (error) {
+    console.log(error);
+  }
+})();
 
-const usercard = function(json){
-    json.forEach(user => {
-        let sectionTag = document.querySelector('#cardsContainer');
-        let cardDiv = document.createElement('div');
-        cardDiv.classList.add('card', 'user');
-        cardDiv.innerHTML = `
+const usercard = function (json) {
+  json.forEach((user) => {
+    let sectionTag = document.querySelector("#cardsContainer");
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "user");
+    cardDiv.innerHTML = `
             <div class="user-img">
                 <img src="../images/${user.img}" class="card-img-top">
             </div>
-            <div class="card-body">
+            <div class="card-body bg-white bg-opacity-50">
                 <h6 class="card-title m-0">${user.fname} ${user.lname}</h6>
                 <p class="m-0"><span class="badge text-success">Room:${user.room}</span> - <span
                     class="badge text-success">EXT.:${user.extNumber}</span>
@@ -30,52 +30,53 @@ const usercard = function(json){
                 </div>
             </div>
         `;
-        sectionTag.appendChild(cardDiv);
+    sectionTag.appendChild(cardDiv);
+  });
+};
+
+let updateClickBtn = function () {
+  document.querySelectorAll(".edit-btn").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      event.preventDefault();
+      const userId = event.currentTarget.getAttribute("data-id");
+      const dataUpdate = (async function () {
+        try {
+          const response = await fetch(
+            `../api/selectSpecificUser.php?id=${userId}`
+          );
+          const userData = await response.json();
+          dataForUpdate(userData);
+          console.log(userData);
+        } catch (error) {
+          console.log("error");
+        }
+      })();
     });
-}
+  });
+};
 
-let updateClickBtn = function(){
-    document.querySelectorAll('.edit-btn').forEach(btn => {
-        btn.addEventListener('click', function(event) {
-            event.preventDefault();
-            const userId = event.currentTarget.getAttribute('data-id');
-            const dataUpdate = async function(){
-                try {
-                    const response = await fetch(`../api/selectSpecificUser.php?id=${userId}`);
-                    const userData = await response.json();
-                    dataForUpdate(userData);
-                    console.log(userData);
-                } catch(error) {
-                    console.log("error");
-                }
-            }();
-        });
+function dataForUpdate(userData) {
+  userData.forEach((key) => {
+    document.getElementById("userID").value = key.id;
+    document.getElementById("updatefname").value = key.fname;
+    document.getElementById("updatelname").value = key.lname;
+    document.getElementById("updateuseremail").value = key.email;
+    document.getElementById("updateroom").value = parseInt(key.room);
+    document.getElementById("updatepass").value = parseInt(key.password);
+
+    // document.getElementById('userimg').value = userData.img
+  });
+}
+const deleteClickBtn = function () {
+  document.querySelectorAll(".delete-btn").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      const userId = event.currentTarget.getAttribute("data-id");
+      document.getElementById("deleteButtn").value = userId;
     });
-}
+  });
+};
 
-function dataForUpdate(userData){
-    userData.forEach(key => {
-        
-        document.getElementById('userID').value = key.id
-        document.getElementById('updatefname').value = key.fname
-        document.getElementById('updatelname').value = key.lname
-        document.getElementById('updateuseremail').value = key.email
-        document.getElementById('updateroom').value = parseInt(key.room) 
-        document.getElementById('updatepass').value =parseInt( key.password)
-       
-        // document.getElementById('userimg').value = userData.img
-    })
-}
-    const deleteClickBtn = function(){
-      document.querySelectorAll('.delete-btn').forEach(btn => {
-        btn.addEventListener('click', function(event) {
-            const userId = event.currentTarget.getAttribute('data-id');
-            document.getElementById('deleteButtn').value =userId
-        });
-      });
-    }
-
-    // document.getElementById('editForm').addEventListener('submit',
+// document.getElementById('editForm').addEventListener('submit',
 //   async function(event) {
 //     event.preventDefault();
 //     const formData = new FormData(this);
--- script/showproductions.js
+++ script/showproductions.js
@@ -1,31 +1,28 @@
-const startpage = async function(){
-    try {
-        const response = await fetch('../api/productsAPI.php');
-        const json = await response.json();
-        productCard(json);
-        updateClickBtn();
-        deleteClickBtn();
-    } catch(error) {
-        console.log(error);
+const startpage = async function () {
+  try {
+    const response = await fetch("../api/productsAPI.php");
+    const json = await response.json();
+    productCard(json);
+    updateClickBtn();
+    deleteClickBtn();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startpage();
+
+let sectionTag = document.querySelector("#cardsProducations");
+const productCard = function (json) {
+  json.forEach((production) => {
+    let classn = "btn-primary";
+    let classb = "fa-check";
+    if (production.available == "non available") {
+      (classn = "btn-danger"), (classb = "fa-ban");
     }
-    };
 
-    startpage();
-
-    let sectionTag = document.querySelector('#cardsProducations');
-    const productCard = function(json){
-    json.forEach(production => {
-        
-        let classn="btn-primary";
-        let classb="fa-check";
-        if(production.available == "non available"){
-            classn= "btn-danger",
-            classb= "fa-ban"
-        }
-
-
-        sectionTag.innerHTML += `
-        <div class="myorder_card card mt-3 me-2 py-3 shadow-sm">
+    sectionTag.innerHTML += `
+        <div class="myorder_card card bg-white bg-opacity-10 mt-3 me-2 py-3 shadow-sm">
         <a href="#" title="edit" class="edit_order btn btn-warning edit-btn" category="${production.category_id}" data-id="${production.id}" data-bs-toggle="modal"
           data-bs-target="#updateModal"><i class="fa-solid fa-pen-to-square"></i></a>
         <a href="#" title="availability" class="available_order btn ${classn}" onclick="changeStatus(${production.id},'${production.available}')" data-id="${production.id}" ><i class="fa-solid ${classb}"></i></a>
@@ -36,196 +33,207 @@ const startpage = async function(){
         </div>
       </div>
         `;
+  });
+};
 
-
+let updateClickBtn = function () {
+  document.querySelectorAll(".edit-btn").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      changeSelect(event.currentTarget.getAttribute("category"));
+      event.preventDefault();
+      const userId = event.currentTarget.getAttribute("data-id");
+      const dataUpdate = (async function () {
+        try {
+          const response = await fetch(
+            `../api/selectSpecificProduct.php?id=${userId}`
+          );
+          const userData = await response.json();
+          dataForUpdate(userData);
+        } catch (error) {
+          console.log("error");
+        }
+      })();
     });
+  });
+};
+
+function dataForUpdate(userData) {
+  userData.forEach((key) => {
+    document.getElementById("productName").value = key.name;
+    document.getElementById("productPrice").value = parseInt(key.price);
+    document.getElementById("productid").value = parseInt(key.id);
+  });
+}
+const deleteClickBtn = function () {
+  document.querySelectorAll(".delete-btn").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      const userId = event.currentTarget.getAttribute("data-id");
+      document.getElementById("deleteButtn").value = userId;
+    });
+  });
+};
+
+async function changeStatus(id, Status) {
+  if (Status == "available") {
+    Status = "non available";
+  } else {
+    Status = "available";
+  }
+  const response = await fetch(
+    `../api/selectSpecificProduct.php?id=${id}&status=${Status}`
+  );
+  element = document.querySelectorAll(`a[data-id="${id}"]`)[1];
+  element.firstChild.classList.toggle("fa-ban");
+  element.firstChild.classList.toggle("fa-check");
+  element.classList.toggle("btn-primary");
+  element.classList.toggle("btn-danger");
 }
 
-let updateClickBtn = function(){
-    document.querySelectorAll('.edit-btn').forEach(btn => {
-        btn.addEventListener('click', function(event) {
-            changeSelect(event.currentTarget.getAttribute('category'));
-            event.preventDefault();
-            const userId = event.currentTarget.getAttribute('data-id');
-            const dataUpdate = async function(){
-                try {
-                    const response = await fetch(`../api/selectSpecificProduct.php?id=${userId}`);
-                    const userData = await response.json();
-                    dataForUpdate(userData);
-                } catch(error) {
-                    console.log("error");
-                }
-            }();
-        });
-    });
+function changeSelect(id_category) {
+  Array.from(
+    document.querySelectorAll(".categorySelector")[1].children
+  ).forEach((option) => {
+    option.removeAttribute("selected");
+  });
+  let i = 0;
+  while (true) {
+    if (
+      document.querySelectorAll(".categorySelector")[1].children[i].value ==
+      id_category
+    ) {
+      document
+        .querySelectorAll(".categorySelector")[1]
+        .children[i].setAttribute("selected", "selected");
+      break;
+    }
+    ++i;
+  }
 }
 
-function dataForUpdate(userData){
-    userData.forEach(key => {
-        
-        document.getElementById('productName').value = key.name
-        document.getElementById('productPrice').value = parseInt(key.price);
-        document.getElementById('productid').value = parseInt(key.id);
+let selectors = document.querySelectorAll(".categorySelector");
+
+const getCategories = async function () {
+  try {
+    const response = await fetch(`../api/categoryAPI.php`);
+    const category = await response.json();
+
+    selectors.forEach((selector) => {
+      for (let i = 0; category.length > i; ++i) {
+        selector.innerHTML += `
+                <option value="${category[i].id}">${category[i].name}</option>`;
+      }
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+getCategories();
+
+async function insertcategory() {
+  let formAddCategory = document.getElementById("addCategoryForm");
+  if (formAddCategory.checkValidity()) {
+    const fileInput = document.getElementById("addcategory");
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append("name", document.getElementById("category").value);
+    formData.append("img", file);
+
+    fetch("../api/insertcategory.php", {
+      method: "POST",
+      body: formData,
     })
-}
-    const deleteClickBtn = function(){
-      document.querySelectorAll('.delete-btn').forEach(btn => {
-        btn.addEventListener('click', function(event) {
-            const userId = event.currentTarget.getAttribute('data-id');
-            document.getElementById('deleteButtn').value =userId
-        });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response from server:", data);
       });
-    }
 
-    async function changeStatus(id,Status){
-        if(Status=="available"){Status="non available"}else{Status="available"}
-        const response = await fetch(`../api/selectSpecificProduct.php?id=${id}&status=${Status}`);
-        element = document.querySelectorAll(`a[data-id="${id}"]`)[1];
-        element.firstChild.classList.toggle("fa-ban");
-        element.firstChild.classList.toggle("fa-check");
-        element.classList.toggle("btn-primary");
-        element.classList.toggle("btn-danger");
+    setTimeout(() => {
+      selectors[0].innerHTML = "";
+      selectors[1].innerHTML = "";
+      getCategories();
+    }, 100);
+    bootstrap.Modal.getOrCreateInstance(
+      document.getElementById("categoryModal")
+    ).hide();
+  }
+}
 
-    }
+async function senddata() {
+  let formAddProduct = document.getElementById("formInsert");
+  if (formAddProduct.checkValidity()) {
+    const fileInput = document.getElementById("product_imgAdd");
+    const file = fileInput.files[0];
 
-    function changeSelect(id_category){
-        Array.from(document.querySelectorAll(".categorySelector")[1].children).forEach(option => {
-            option.removeAttribute("selected");
-        });
-        let i=0
-        while(true){
-            if(document.querySelectorAll(".categorySelector")[1].children[i].value==id_category){
-                document.querySelectorAll(".categorySelector")[1].children[i].setAttribute("selected", "selected");
-                break;
-            }
-            ++i;
-        }
+    const formData = new FormData();
 
-    }
+    formData.append("name", document.getElementById("productNameAdd").value);
+    formData.append(
+      "category_id",
+      parseInt(document.getElementById("selectorCategoryAdd").value)
+    );
+    formData.append(
+      "price",
+      parseInt(document.getElementById("productPriceAdd").value)
+    );
+    formData.append("available", "available");
+    formData.append("img", file);
 
+    fetch("../api/insertProduct.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response from server:", data);
+      });
 
+    setTimeout(() => {
+      sectionTag.innerHTML = "";
+      startpage();
+    }, 100);
+    bootstrap.Modal.getOrCreateInstance(
+      document.getElementById("addModal")
+    ).hide();
+  }
+}
 
-    let selectors = document.querySelectorAll(".categorySelector");
-    
-    const getCategories =async function(){
-    try {
-        const response = await fetch(`../api/categoryAPI.php`);
-        const category = await response.json();
+async function updateProduct() {
+  let formUpdateProduct = document.getElementById("updateform");
 
+  if (formUpdateProduct.checkValidity()) {
+    const fileInput = document.getElementById("product_img");
+    const file = fileInput.files[0];
 
-        selectors.forEach(selector => {
-            for(let i=0 ; category.length>i ;++i){
-                selector.innerHTML +=`
-                <option value="${category[i].id}">${category[i].name}</option>`}
-        });
+    const formData = new FormData();
 
-    } catch(error) {
-        console.log("error");
-    }
-    };
-    
-    getCategories();
+    formData.append("id", parseInt(document.getElementById("productid").value));
+    formData.append("name", document.getElementById("productName").value);
+    formData.append(
+      "category_id",
+      parseInt(document.getElementById("productCategory").value)
+    );
+    formData.append(
+      "price",
+      parseInt(document.getElementById("productPrice").value)
+    );
+    formData.append("available", "available");
+    formData.append("img", file);
 
-    async function insertcategory(){
-        let formAddCategory = document.getElementById('addCategoryForm');
-        if(formAddCategory.checkValidity()){
-        const fileInput = document.getElementById('addcategory');
-        const file = fileInput.files[0];
-        const formData = new FormData();
-        formData.append('name', document.getElementById("category").value);
-        formData.append('img', file);
-
-
-        fetch('../api/insertcategory.php', {
-        method: 'POST',
-        body: formData
-        }).then(response => response.json())
-        .then(data => {
-        console.log('Response from server:', data);
-        })
-
-        setTimeout(() => {
-            selectors[0].innerHTML ="";
-            selectors[1].innerHTML ="";
-            getCategories();
-        }, 100);
-        bootstrap.Modal.getOrCreateInstance(document.getElementById("categoryModal")).hide();
-        }
-        }
-
-   
-
-        async function senddata(){
-            let formAddProduct = document.getElementById('formInsert');
-            if(formAddProduct.checkValidity()){
-            const fileInput = document.getElementById('product_imgAdd');
-            const file = fileInput.files[0];
-    
-            const formData = new FormData();
-            
-    
-            formData.append('name', document.getElementById("productNameAdd").value);
-            formData.append('category_id', parseInt(document.getElementById("selectorCategoryAdd").value));
-            formData.append('price', parseInt(document.getElementById("productPriceAdd").value));
-            formData.append('available', "available");
-            formData.append('img', file);
-    
-    
-            fetch('../api/insertProduct.php', {
-            method: 'POST',
-            body: formData
-            }).then(response => response.json())
-            .then(data => {
-            console.log('Response from server:', data);
-            })
-
-            setTimeout(() => {
-                sectionTag.innerHTML="";
-                startpage();    
-            }, 100);
-            bootstrap.Modal.getOrCreateInstance(document.getElementById("addModal")).hide();
-
-            }
-
-            }
-
-
-
-            async function updateProduct(){
-                let formUpdateProduct = document.getElementById('updateform');
-
-                if(formUpdateProduct.checkValidity()){
-                const fileInput = document.getElementById('product_img');
-                const file = fileInput.files[0];
-        
-                const formData = new FormData();
-                
-                formData.append('id', parseInt(document.getElementById("productid").value));
-                formData.append('name', document.getElementById("productName").value);
-                formData.append('category_id', parseInt(document.getElementById("productCategory").value));
-                formData.append('price', parseInt(document.getElementById("productPrice").value));
-                formData.append('available', "available");
-                formData.append('img', file);
-        
-        
-                fetch('../api/updateProduct.php', {
-                method: 'POST',
-                body: formData
-                }).then(response => response.json())
-                .then(data => {
-                console.log('Response from server:', data);
-                })
-                setTimeout(() => {
-                    sectionTag.innerHTML="";
-                    startpage();    
-                }, 100);
-                bootstrap.Modal.getOrCreateInstance(document.getElementById("updateModal")).hide();
-                }
-                
-                }
-
-
-     
-
-
-
+    fetch("../api/updateProduct.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Response from server:", data);
+      });
+    setTimeout(() => {
+      sectionTag.innerHTML = "";
+      startpage();
+    }, 100);
+    bootstrap.Modal.getOrCreateInstance(
+      document.getElementById("updateModal")
+    ).hide();
+  }
+}
