a12a4816d3b80a6337027daf99040c84ddb26f36
amr-hc <amro7275@gmail.com>
2024-03-16 13:40:37 +0200

    reset password & products & confirm code

--- api/confirmCode.php
+++ api/confirmCode.php
@@ -1,18 +1,29 @@
 <?php
 
   session_start();
+  include("db.php");
   if(isset($_SESSION['code']) && $_SESSION['code']==$_POST["code"]){
-  
-    include("db.php");
+
     $mydb = new DB();
     $email=$_SESSION['email'];
     $password=md5($_POST['password']);
     $mydb->update_data('customers',"password = '$password'","email='{$email}'");
-    session_destroy();
+    
+    $response = [
+      'status' => 'success',
+      'message' => 'Password changed'];
+      session_destroy();
 
+}else{
+  $response = [
+    'status' => 'failed',
+    'message' => 'Failed changing password'];
 
 }
 
+
+
+echo json_encode($response);
 
 
 ?>
--- api/resetPassword.php
+++ api/resetPassword.php
@@ -4,18 +4,17 @@ session_start();
 
 include("db.php");
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
  
-//required files
+
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
  
 
-$length = 10; // Change this to your desired length
+$length = 10; 
 $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 $randomString = substr(str_shuffle($characters), 0, $length);
 
@@ -43,45 +42,31 @@ if($data){
   $_SESSION['code'] = $forgetCode;
   $_SESSION['email'] = $_POST["email"];
   
-
-
-
-//Create an instance; passing `true` enables exceptions
-if (isset($_POST["send"])) {
  
   $mail = new PHPMailer(true);
  
     //Server settings
-    $mail->isSMTP();                              //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;             //Enable SMTP authentication
-    $mail->Username   = 'amr.abdullah.elrefaey@gmail.com';   //SMTP write your email
-    $mail->Password   = 'cflz damj inrm odgx';      //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit SSL encryption
+    $mail->isSMTP();                           
+    $mail->Host       = 'smtp.gmail.com';     
+    $mail->SMTPAuth   = true;            
+    $mail->Username   = 'amr.abdullah.elrefaey@gmail.com'; 
+    $mail->Password   = 'cflz damj inrm odgx';     
+    $mail->SMTPSecure = 'ssl';         
     $mail->Port       = 465;                                    
  
-    //Recipients
-    // $mail->setFrom( "admin@cafeteria.com", "admin"); // Sender Email and name
-    $mail->addAddress($_POST["email"]);     //Add a recipient email  
-    $mail->addReplyTo($_POST["email"], "Customer"); // reply to sender email
+  
+    $mail->addAddress($_POST["email"]);    
+    $mail->addReplyTo($_POST["email"], "Customer");
  
-    //Content
-    $mail->isHTML(true);               //Set email format to HTML
-    $mail->Subject = "Reset Your account password in cafeteria.com";   // email subject headings
-    $mail->Body    = "Your link to forget password : <a href='http://localhost/Cafiteria-php-project/api/confirmCode.html?code=$forgetCode'>Click here to reset your password</a>"; //email message
+    
+    $mail->isHTML(true);              
+    $mail->Subject = "Reset Your account password in cafeteria.com";   
+    $mail->Body    = "Your link to forget password : <a href='http://localhost/Cafiteria-php-project/html/confirmCode.html?code=$forgetCode'>Click here to reset your password</a>"; //email message
       
-    // Success sent message alert
     $mail->send();
 
-    // if ($mail->send()) {
-    //     echo 'Email sent successfully.';
-    // } else {
-    //     echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
-    // }
 
 
-    
-}
 
 }else{
   $response = [
